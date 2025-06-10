4fce442277a94598de52c536765357c596961e7b
Rodrigo Sarno <107512180+RodsSarno@users.noreply.github.com>
2025-06-09 23:08:56 -0300

    Delete app directory

--- app/models/client.py
+++
@@ -1,7 +0,0 @@
-from pydantic import BaseModel
-
-
-class Client(BaseModel):
-    id: str
-    connected: bool = False
-    last_activity: str = ""
--- app/models/server.py
+++
@@ -1,6 +0,0 @@
-from pydantic import BaseModel
-
-class Server(BaseModel):
-    id: str
-    status: str = "offline"
-    connected_clients: int = 0
