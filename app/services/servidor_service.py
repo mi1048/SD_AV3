7d87e019e0b7f0ae917c1f7b481c4935e1eb6852
Rodrigo Sarno <107512180+RodsSarno@users.noreply.github.com>
2025-06-14 20:57:02 -0300

    Add files via upload

--- app/services/servidor_service.py
+++ app/services/servidor_service.py
@@ -1,6 +1,6 @@
 from app.models.servidor import Servidor
 from datetime import datetime
-
+from bson import ObjectId  
 
 def serializar_mongo(doc):
     if not doc:
@@ -21,3 +21,7 @@ class ServicoServidor:
     def listar_servidores(self):
         # Serializa cada documento para evitar erro com ObjectId
         return [serializar_mongo(s) for s in Servidor.colecao().find({})]
+
+    def obter_nome_servidor(self, servidor_id: str):
+        servidor = Servidor.colecao().find_one({"_id": ObjectId(servidor_id)})
+        return servidor["nome"] if servidor else None
